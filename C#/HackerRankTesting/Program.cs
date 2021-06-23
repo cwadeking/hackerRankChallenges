8eecbaee088b1ec22ddc3ffa7816f74ae9e8e274
cwadeking <charles.w.king.cwk@gmail.com>
2021-06-23 10:21:36 -0500

    Started a few months ago with C# challenges. Will continue mostly in JavaScript from here on out.

---
+++ C#/HackerRankTesting/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTesting
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            //int[] arr = new int[5];
+            //arr.Append(1);
+            //arr.Append(2);
+            //arr.Append(3);
+            //arr.Append(4);
+            //arr.Append(5);
+
+            //Result.staircase(6);
+            //Console.ReadLine();
+
+            List<List<int>> arr = new List<List<int>>();
+            //arr.Add(new List<int> { 4 });
+            //arr.Add(new List<int> { -1, 1, -7, -8 });
+            //arr.Add(new List<int> { -10, -8, -5, -2 });
+            //arr.Add(new List<int> { 0, 9, 7, -1 });
+            //arr.Add(new List<int> { 4, 4, -2, 1 });
+            arr.Add(new List<int> { 3 });
+            arr.Add(new List<int> { 11, 2, 4 });
+            arr.Add(new List<int> { 4, 5, 6 });
+            arr.Add(new List<int> { 10, 8, -12 });
+
+            int result = Result.diagonalDifference(arr);
+
+            Console.ReadLine();
+
+
+
+        }
+                    
+    }
+}
---
+++ C#/HackerRankTesting/Result.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTesting
+{
+    static class Result
+    {
+
+        //static void miniMaxSum(int[] arr)
+        //{
+        //    long minSum = 0;
+        //    long maxSum = 0;
+        //    Array.Sort(arr, 0, arr.Length);
+        //    for(int i = 0; i < arr.Length - 1; i++)
+        //    {
+        //        minSum += arr[i];
+        //    }
+        //    Array.Reverse(arr);
+        //    for (int i = 0; i < arr.Length - 1; i++)
+        //    {
+        //        maxSum += arr[i];
+        //    }
+        //    Console.WriteLine(minSum + " " + maxSum);
+        //}
+        //public static void staircase(int n)
+        //{
+        //    int poundCounter = 1;
+        //    while(n > 0)
+        //    {
+        //        string emptySpaces = "";
+        //        for(int i = 0; i < n -1; i++)
+        //        {
+        //            emptySpaces += " ";
+        //        }
+        //        for(int i = 0; i < poundCounter; i++)
+        //        {
+        //            emptySpaces += "#";
+        //        }
+        //        poundCounter++;
+        //        Console.WriteLine(emptySpaces);
+        //        n -= 1;
+        //        emptySpaces = "";
+        //    }
+
+        //}
+
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            var firstDiagonal = 0;
+            var secondDiagonal = 0;
+            var diagonalCount = arr.Count - 1;
+            var result = 0;
+
+            for (var i = 1; i <= diagonalCount; i++)
+            {
+                var differential = i - 1;
+                firstDiagonal += arr[i][differential];
+                secondDiagonal += arr[diagonalCount - differential][i - 1];
+            }
+
+            result = firstDiagonal - secondDiagonal;
+            result = Math.Abs(result);
+            return result;
+        }
+    }
+}
