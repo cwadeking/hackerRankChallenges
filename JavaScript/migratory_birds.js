4c5d52eb44795f91e0b174fc011fa089f28604f0
cwadeking <charles.w.king.cwk@gmail.com>
2021-07-28 15:42:08 -0500

    created solution for salesByMatch which passes all test cases.  when importing a helper function, the current test cases helped me realize that my helper function did not account for some edge cases.  Fixed that helper function.

--- JavaScript/migratory_birds.js
+++ JavaScript/migratory_birds.js
@@ -45,10 +45,65 @@ Steps to solve.
 let testArray= [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,20,1,2,3,4,5,6,7,8,9,30,1,2,3,4,5,6,7,8,9,40,1,2,3,4,5,6,7,8,9,50,1,2,3,4,5,6,7,8,9,60,1,2,3,4,5,6,7,8,9,70,1,2,3,4,5,6,7,8,9,80,1,2,3,4,5,6,7,8,9,90,1,2,3,4,5,6,7,8,9,100,1,2,3,4,5,6,7,8,9,110,1,2,3,4,5,6,7,8,9,120,1,2,3,4,5,6,7,8,9,130,1,2,3,4,5,6,7,8,9,140,1,2,3,4,5,6,7,8,9,150,1,2,3,4,5,6,7,8,9,160,1,2,3,4,5,6,7,8,9,170,1,2,3,4,5,6,7,8,9,180,1,2,3,4,5,6,7,8,9,190,1,2,3,4,5,6,7,8,9,200];
 let testArrayTwo = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,20,1,2,3,4,5,6,7,8,9,30,1,2,3,4,5,6,7,8,9,40,1,2,3,4,5,6,7,8,9,50,1,2,3,4,5,6,7,8,9,60,1,2,3,4,5,6,7,8,9,70,1,2,3,4,5,6,7,8,9,80,1,2,3,4,5,6,7,8,9,90,1,2,3,4,5,6,7,8,9,100];
 
+function migratoryBirds(arrayOfIntegers){
+  arrayOfIntegers.sort(function(a, b){
+    return a - b;
+  });  
+  let arrayOfObjects = findFrequencyOfSortedArray(arrayOfIntegers);
+  let frequencyOfObjects = findHighestFrequencyInArrayOfObjects(arrayOfObjects);
+  console.log(frequencyOfObjects[0].typeValue);
+  return frequencyOfObjects[0].typeValue;
+}
+
+function findHighestFrequencyInArrayOfObjects(arrayOfObjects){
+  let currentHighestFrequency = 0;
+  
+  for(let i = 0; i < arrayOfObjects.length; i ++){
+    if(arrayOfObjects[i]["frequency"] > currentHighestFrequency){
+      currentHighestFrequency = arrayOfObjects[i]["frequency"];
+    }
+  }
+  
+  let arrayOfHighestFrequencyObjects = arrayOfObjects.filter(function(el){
+    if(el.frequency === currentHighestFrequency){
+      return true;
+    }
+  })
+  
+  return arrayOfHighestFrequencyObjects;
+}
+
+function findFrequencyOfSortedArray(sortedArray){
+  let newArrayOfObjects = [];
+  let currentValue = sortedArray[0];
+  let counter = 0;
+  
+  if(sortedArray.length > 0){
+    for(let i = 1; i <= sortedArray.length + 1; i++){      
+      if(currentValue === sortedArray[i - 1]){
+        if(i <= sortedArray.length - 1){// && currentValue === sortedArray[i - 1]){
+          counter++;
+        }
+        if(i === sortedArray.length){
+          counter++;
+        }
+      }else if(currentValue !== sortedArray[i - 1]){
+        newArrayOfObjects.push({"typeValue":currentValue, "frequency": counter});
+        currentValue = sortedArray[i - 1];
+        counter = 1;
+      }
 
 
-let startTime = new Date().getTime();
-migratoryBirds(testArrayTwo);
-let endTime = new Date().getTime();
-let totalTime = endTime - startTime;
-console.log(`${totalTime} milliseconds`);
+    }
+  }
+
+  return newArrayOfObjects;
+}
+
+exports.findFrequencyOfSortedArray = findFrequencyOfSortedArray;
+
+// let startTime = new Date().getTime();
+//migratoryBirds(testArrayTwo);
+// let endTime = new Date().getTime();
+// let totalTime = endTime - startTime;
+// console.log(`${totalTime} milliseconds`);
---
+++ JavaScript/sales_by_match.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { findFrequencyOfSortedArray } = require('./migratory_birds');
+
+console.log(findFrequencyOfSortedArray);
+/*
+https://www.hackerrank.com/challenges/sock-merchant/problem
+
+Function Description
+
+Complete the sockMerchant function in the editor below.
+
+sockMerchant has the following parameter(s):
+
+int n: the number of socks in the pile
+int ar[n]: the colors of each sock
+Returns
+
+int: the number of pairs
+
+Steps to complete-
+
+1) Sort the array.
+2) Compare each digit to the previous.
+3) Create a counter for pairs and a counter for current value.
+4) Have a conditional check that resets the counter for current value
+if it's 2, and adds to the pair counter.
+*/
+
+let testArray = [44, 55, 11, 15, 4, 72, 72];//, 91, 80, 14, 43, 78, 70, 75, 36, 83, 78, 91, 17, 17, 54, 65, 60, 21, 58, 98, 87, 45, 75, 97, 81, 18, 51, 43, 84, 54, 66, 10, 44, 45, 23, 38, 22, 44, 65, 9, 78, 42, 100, 94, 58, 5, 11, 69, 26, 20, 19, 64, 64, 93, 60, 96, 10, 10, 39, 94, 15, 4, 3, 10, 1, 77, 48, 74, 20, 12, 83, 97, 5, 82, 43, 15, 86, 5, 35, 63, 24, 53, 27, 87, 45, 38, 34, 7, 48, 24, 100, 14, 80, 54];//4 pairs
+
+sockMerchant(testArray.length, testArray);
+function sockMerchant(n, ar) {
+  if(ar.length <= 1){
+    return 0;
+  }
+  ar.sort(function(a, b){
+    return a - b;
+  })
+
+  let arrayOfFrequencyObjects = findFrequencyOfSortedArray(ar);
+  let numberOfPairs = findNumberOfPairsInObjectArray(arrayOfFrequencyObjects);
+}
+
+function findNumberOfPairsInObjectArray(objectsArray){
+  let numberOfPairs = 0;
+  for(let i = 0; i < objectsArray.length; i++){
+    numberOfPairs += Math.floor(objectsArray[i].frequency / 2);
+  }
+  return numberOfPairs;
+}
